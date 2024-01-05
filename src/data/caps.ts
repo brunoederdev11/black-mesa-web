ee06e03f50d0cf7db5cf55d9e4982d0c410428c9
Eder Bruno <bruno.miranda.espinoza@gmail.com>
2024-01-04 22:03:35 -0500

    add optimized images

--- src/data/caps.ts
+++ src/data/caps.ts
@@ -3,131 +3,131 @@ export const CAPS = [
         title: "Bienvenido a Black Mesa",
         parrafo:
             "Gordon Freeman va a su puesto de trabajo, utilizando el sistema de transporte del complejo. Parte desde el sector de baja seguridad, donde se encuentran las habitaciones y se dirige al sector C donde se encuentra la sección de Materiales anómalos en la cual trabaja.",
-        image: "../../public/maxresdefault (3).jpg",
+        image: "../../public/maxresdefault (3).webp",
         alt: "Sistema ferroviario de black mesa",
     },
     {
         title: "Materiales anómalos",
         parrafo:
             "Freeman llega y se equipa su traje protector para llevar a cabo el experimento en la cámara de pruebas. Al parecer, llega bastante tarde, y todos sus colegas se lo terminan reprochando. En el transcurso del camino, ve a un misterioso personaje vestido de traje azul, conversando con un científico. Aparentemente, el primero está instándolo a hacer algo que éste no quiere",
-        image: "../../public/maxresdefault (2).jpg",
+        image: "../../public/maxresdefault (2) (1).webp",
         alt: "Recepción de la camara de pruebas",
     },
     {
         title: "Consecuencias imprevistas",
         parrafo:
             "El experimento fracasa, abriendo un portal por donde cruzan los extraterrestres de la dimensión XEN de donde obtuvieron la muestra. El jugador tiene su primer enfrentamiento contra criaturas como el headcrab (en español: `cangrejo asaltacabezas`) y algunas criaturas más como los vortigaunts. Consigue escapar del sector mediante unos viejos pasajes industriales.",
-        image: "../../public/maxresdefault (4).jpg",
+        image: "../../public/maxresdefault (4).webp",
         alt: "Camara de pruebas destruida",
     },
     {
         title: "Complejo de oficinas",
         parrafo:
             "Freeman tiene como objetivo llegar a la superficie para buscar la ayuda de las fuerzas armadas del gobierno, recién llegadas a la superficie del complejo. Para lograrlo debe atravesar un complejo de oficinas infestadas de alienígenas, pero también cuenta con aliados.",
-        image: "../../public/maxresdefault (5).jpg",
+        image: "../../public/maxresdefault (5).webp",
         alt: "Oficinas afectadas por el incidente",
     },
     {
         title: "Tenemos elementos hostiles",
         parrafo:
             "Comienza la llegada de los soldados de la HECU que tratan de tapar el caso eliminando a los sobrevivientes, Gordon Freeman incluido. Cuando éste comprende que la salida no está en la superficie, vuelve a meterse en lo profundo del complejo, donde se encuentra con un colega científico que le explica que el Equipo Lambda es el único que puede acabar con este desastre, indicándole cómo llegar hasta su ubicación. Gordon se pone en marcha atravesando zonas industriales, el recorrido más óptimo debido a los numerosos fallos de seguridad.",
-        image: "../../public/ctvrtgw.webp",
+        image: "../../public/ctvrtgw (1).webp",
         alt: "Superficie en proceso de ser tomada por el ejercito",
     },
     {
         title: "Foso explosivo",
         parrafo:
             "Gordon Freeman recorre los silos nucleares de Black Mesa, donde llega a una gran cámara donde se fabrican motores de cohetes. Debe utilizar dichos motores para poder aniquilar a los tres grandes tentáculos xenianos que le impiden poder continuar. Escapará de la zona por una tubería gigante de agua, que termina cediendo y arrojándolo a un pequeño depósito",
-        image: "../../public/james-n-blast-bit.jpg",
+        image: "../../public/james-n-blast-bit.webp",
         alt: "Tentáculos atacando a un cientifico",
     },
     {
         title: "Energía",
         parrafo:
             "Freeman tendrá que suministrar energía a la pista de raíles para continuar su progreso, pero esta misión se ve interrumpida por un contratiempo: Un formidable general xeniano blindado llamado Gargantua acecha en el medio de las vías",
-        image: "../../public/maxresdefault (6).jpg",
+        image: "../../public/maxresdefault (6).webp",
         alt: "Gargantua siendo electrocutado",
     },
     {
         title: "Sobre raíles",
         parrafo:
             "Gordon utiliza el viejo sistema de rieles en desuso para llegar a la superficie, donde debe llegar a una plataforma de lanzamiento. Una vez allí, tendrá que lanzar un cohete que libere el satélite Lambda.",
-        image: "../../public/maxresdefault (7).jpg",
+        image: "../../public/maxresdefault (7).webp",
         alt: "Railes subterraneos con dirección al silo",
     },
     {
         title: "Arresto",
         parrafo:
             "Freeman llega a zonas industriales subterráneas anegadas y a los almacenes abandonados controlados por el ejército, además de estar infestado de xenianos. Al final del capítulo, justo antes de por fin alcanzar la superficie, Gordon es capturado finalmente, en una emboscada realizada por los soldados.",
-        image: "../../public/polygon_media_06.0.jpg",
+        image: "../../public/polygon_media_06.0.webp",
         alt: "Black ops patrullando la zona",
     },
     {
         title: "Procesamiento de residuos",
         parrafo:
             "Privado de todas sus armas, Gordon es abandonado en un compresor de residuos y dado por muerto. Sin embargo, él consigue escapar del lugar y salir a la superficie, aunque en una zona periférica. Para volver al recorrido, recorre el inhóspito y algo macabro centro de tratamiento de residuos, plagado de ciertos xenianos roedores.",
-        image: "../../public/c2a4_release11_004.jpg",
+        image: "../../public/c2a4_release11_004.webp",
         alt: "Tanques de residuos toxicos",
     },
     {
         title: "Ética dudosa",
         parrafo:
             "En este capítulo de transición, Freeman finalmente llega a un laboratorio de prueba de armamentos, tomado por soldados, en donde hay algunos equipos láser como objeto de examen. Para escapar, debe encontrar algún colega científico para poder pasar la retina de identificación y salir al exterior. Sin embargo, una vez afuera, todo está infestado de soldados y xenianos por todas partes…",
-        image: "../../public/maxresdefault (8).webp",
+        image: "../../public/maxresdefault (8) (1).webp",
         alt: "Soldados entrando por el techo de las instalaciones",
     },
     {
         title: "Tensión en la superficie",
         parrafo:
             "Por fin, Gordon Freeman alcanza la superficie. Avanza a través de un vasto desierto y por la zona montañosa, situada en las afueras de Black Mesa. El ejército controla cada sector y tratarán de eliminar a Gordon por todos los medios necesarios.",
-        image: "../../public/56348fbb25826.webp",
+        image: "../../public/56348fbb25826 (1).webp",
         alt: "Paisaje de zona montañosa de las afueras de black mesa",
     },
     {
         title: `"¡Olvídense de Freeman!"`,
         parrafo:
             "Mientras Gordon está por llegar al Complejo Lambda, los soldados, al ver la situación, declaran la retirada para no sufrir más bajas y así la Fuerza Aérea pueda comenzar a bombardear el sitio. Poco a poco, el protagonista se reintroduce en las instalaciones subterráneas de Black Mesa, en busca del equipo Lambda.",
-        image: "../../public/Nq4I7uw.webp",
+        image: "../../public/Nq4I7uw (1).webp",
         alt: "Grupo de soldados con un tanque saliendo de la instalación",
     },
     {
         title: "Núcleo Lambda",
         parrafo:
             "Freeman finalmente consigue llegar al complejo Lambda, pero en este lugar ya reina el caos. Grandes cantidades de xenianos enormes no le permitirán a Gordon Freeman llegar hasta el reactor. En el camino, se irá encontrando con los restos del equipo Lambda, los cuales le irán guiando para que pueda llegar a la cima del reactor, donde lo espera el último miembro del equipo junto con un guardia. Éste individuo, incita a Gordon a enfrascarse en un viaje al mundo fronterizo, para intentar acabar con la criatura que controla a los xenianos.",
-        image: "../../public/lcIIy9ZWEhMbMZgcRZ3AMR.webp",
+        image: "../../public/lcIIy9ZWEhMbMZgcRZ3AMR (1).webp",
         alt: "Núcleo lambda creando un portal hacia xen",
     },
     {
         title: "Xen",
         parrafo:
             "Gordon Freeman es teletransportado a Xen, el mundo fronterizo, bastante diferente de la Tierra. El módulo de salto largo (que le dan antes de ir a Xen como precaución) es indispensable para progresar con éxito, debido a la poca gravedad que hay en el planeta.",
-        image: "../../public/video-game-black-mesa-half-life-wallpaper-preview.webp",
+        image: "../../public/video-game-black-mesa-half-life-wallpaper-preview (1).webp",
         alt: "Flora xeniana",
     },
     {
         title: "El escondite de Gonarch",
         parrafo:
             "En este capítulo, Gordon Freeman tiene que luchar contra Gonarch, la madre de los Headcrabs, una criatura gigante y espantosa con una fuerza descomunal. Sólo matándola Gordon podrá entrar en el segundo teletransportador, esencial para su progreso.",
-        image: "../../public/black-mesa-gonarch-half-life-games-wallpaper-preview.jpg",
+        image: "../../public/black-mesa-gonarch-half-life-games-wallpaper-preview.webp",
         alt: "Gonarch en su guarida",
     },
     {
         title: "Intruso",
         parrafo: `En este tramo, Gordon Freeman entra en una especie de "fábrica" o centro de procesado, donde hay varios esclavos trabajando en la creación de criaturas genéticamente modificadas, como los Grunts, lo que demuestra claramente que estos no son xenianos originales. Gordon tendrá que llevar a cabo difíciles combates para encontrar los 4 teletransportadores restantes.`,
-        image: "../../public/maxresdefault.webp",
+        image: "../../public/maxresdefault (9).webp",
         alt: "El centro de procesado",
     },
     {
         title: "Nihilanth",
         parrafo:
             "Gordon Freeman llega al jefe final: Nihilanth. El líder de los habitantes de Xen; una criatura que controla la teletransportacion de aliens a Black Mesa, y además es una criatura mutilada y con implantes cibernéticos, con un gran poder sobre el teletransporte, que observó el progreso del héroe por todo Xen, insinuándole cosas que no comprenderemos del todo hasta que no juguemos la secuela. Después de una larga y dolorosa lucha entre él y sus subordinados, logra vencer a Nihilanth, concluyendo así el final, pero justo antes de la explosión es teletransportado a otro lugar.",
-        image: "../../public/maxresdefault (1).webp",
+        image: "../../public/maxresdefault (1) (1).webp",
         alt: "Nihilanth en su camara",
     },
     {
         title: "Epílogo - Endgame (Final)",
         parrafo: `Gordon aparece frente a G-Man, quien lo felicita por el trabajo realizado, le explica muy brevemente su situación y le ofrece un "puesto de trabajo", pero si Gordon se niega, el misterioso G-man le ofreceria "una batalla en la que no tendría posibilidad de ganar". Dependiendo de lo que escojamos, el juego terminará con un final distinto. Si aceptamos el trabajo la historia continuará (en el siguiente episodio de la saga), mientras que en el caso de no aceptar el trabajo, seremos teletransportado sin armas frente a un escuadrón de xenianos, dando a entender que Gordon muere.`,
-        image: "../../public/maxresdefault (2).webp",
+        image: "../../public/maxresdefault (2) (2).webp",
         alt: "G-man contandonos todo lo que ocurrio gracias a nosotros",
     },
 ];
